2cb1b910b27dcba49d850c969ef64176fc3c58d1
parsa <parsa@f4836ff4-69f9-4411-9a25-690e74a26bb1>
2016-11-05 11:39:59 +0000

    ACCOUNTING-385

--- accounting/trunk/src/ir/serajsamaneh/accounting/exception/SanadIsNotBalancedException.java
+++ accounting/trunk/src/ir/serajsamaneh/accounting/exception/SanadIsNotBalancedException.java
@@ -1,5 +1,6 @@
 package ir.serajsamaneh.accounting.exception;
 
+import ir.serajsamaneh.accounting.sanadhesabdari.SanadHesabdariEntity;
 import ir.serajsamaneh.core.exception.FatalException;
 import ir.serajsamaneh.core.util.SerajMessageUtil;
 
@@ -10,12 +11,18 @@ public class SanadIsNotBalancedException extends FatalException {
 	 */
 	private static final long serialVersionUID = 563157078681287128L;
 
+	SanadHesabdariEntity entity;
+	
 	public SanadIsNotBalancedException() {
 	}
 
+	public SanadIsNotBalancedException(SanadHesabdariEntity entity) {
+		this.entity=entity;
+	}
+	
 	@Override
 	public String getDesc() {
-		return SerajMessageUtil.getMessage("Sanad_notBalanced");
+		return SerajMessageUtil.getMessage("Sanad_notBalanced", entity);
 	}
 	
 }
